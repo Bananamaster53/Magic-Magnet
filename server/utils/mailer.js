a4a5cd9faf62793441889b6c003bd482d44c7d78
Mátés Marcell <you@example.com>
2026-01-11 21:55:47 +0100

    customImages definition fix

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -2,18 +2,16 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // true a 465-ös porthoz, false minden más porthoz
+  host: 'smtp-relay.brevo.com',
+  port: 587,
+  secure: false, // Az 587-es portnál false kell
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    pass: process.env.EMAIL_PASS // Itt a Brevo API kulcsod lesz
   },
   tls: {
-    // Ez segít, ha a tanúsítvány ellenőrzése akadályozná a kapcsolatot
-    rejectUnauthorized: false 
-  },
-  connectionTimeout: 10000, // 10 másodperc várakozás a kapcsolatra
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = transporter;
